1c8b360899ebad8a445a0f0cbb989ec263e23936
sciscar <90604890+sciscar@users.noreply.github.com>
2023-05-23 12:24:08 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,4 @@
 xhost +local:
 xhost +si:localuser:root
 docker run --security-opt apparmor:unconfined --name examenIntellij --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" rycus86/intellij-idea
+#https://hub.docker.com/r/rycus86/intellij-idea
